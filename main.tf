f5aded929f475431e81ee3fe5a2aa097e63bdc54
flaporta <fab.laporta@gmail.com>
2026-09-12 08:01:13 -0400

    fix main

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 resource "helm_release" "traefik_ingress" {
   name             = "traefik"
-  repository       = "https://github.io"
-  chart            = "traefik"
+  # CHANGE THIS: Update to the correct GitHub Pages chart endpoint
+  chart            = "oci://ghcr.io/traefik/helm/traefik" # Points directly to OCI registry"
   version          = "41.5.0" # Keeps it on a stable chart version for Traefik v3
-  namespace        = "kube-system"
-  create_namespace = false
+  namespace        = "traefik"
+  create_namespace = true
+
 
   # Passes the minimal drop-in configuration file
   values = [
