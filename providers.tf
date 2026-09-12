23c1f6a38cfac28844b7340fb98e65b0d41e50cb
flaporta <fab.laporta@gmail.com>
2026-09-12 07:37:01 -0400

    add code to login to rke2 cluster

--- providers.tf
+++ providers.tf
@@ -1,17 +1,26 @@
 terraform {
   required_version = ">= 1.0.0"
+
   required_providers {
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.0.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
   }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "default"
 }
 
 provider "helm" {
   kubernetes {
-    # Points to your local kubeconfig. 
-    # Modify the path if yours is located elsewhere (e.g., Minikube or Kind)
-    config_path = "~/.kube/config"
+    config_path    = "~/.kube/config"
+    config_context = "default"
   }
 }
